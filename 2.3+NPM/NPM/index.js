74eed20916c854591d6b856c148ab6ffacdbc3cd
Sasukexo <aswinash1002@gmail.com>
2023-07-25 21:15:30 +0530

    superhero package

---
+++ 2.3+NPM/NPM/index.js
@@ -0,0 +1,10 @@
+// var generatename=require("sillyname");
+
+// import generateName from "sillyname"
+// var sillyname = generateName();
+// console.log(sillyname);
+
+
+import superheroes from "superheroes";
+var name = superheroes.random();
+console.log("My name is "+name);
