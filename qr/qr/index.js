7c6443b6115ffaf2e1fb2faaba85310cf3dae2b4
Sasukexo <aswinash1002@gmail.com>
2023-07-26 20:10:40 +0530

    changed index file

--- qr/qr/index.js
+++ qr/qr/index.js
@@ -21,7 +21,7 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+        // Prompt couldn't be rendered in the current environment
     } else {
       // Something else went wrong
     }
